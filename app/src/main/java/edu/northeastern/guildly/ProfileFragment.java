b3ab670cf47014e632c9637b7abf7d408a473ffb
Parwaz Singh Sarao <parwazekam@gmail.com>
2025-03-31 12:53:37 -0400

    changed the profilefragment file to get a popup

--- app/src/main/java/edu/northeastern/guildly/ProfileFragment.java
+++ app/src/main/java/edu/northeastern/guildly/ProfileFragment.java
@@ -6,11 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ProfileFragment extends Fragment {
 
@@ -70,13 +75,40 @@ public class ProfileFragment extends Fragment {
     }
 
     private void setupClickListeners() {
-
+        // When "View More" in habits section is clicked
         habitsViewMore.setOnClickListener(v -> {
-            Intent intent = new Intent(getActivity(), ViewHabitsActivity.class);
-            startActivity(intent);
+            // Show dialog instead of launching an activity
+            showHabitsDialog();
         });
 
+        // Other click listeners can be added here as needed
+    }
 
+    private void showHabitsDialog() {
+        // Get the list of habits similar to HomeFragment
+        List<Habit> userHabits = Arrays.asList(
+                new Habit("Drink 64oz of water", R.drawable.ic_water),
+                new Habit("Workout for 30 mins", R.drawable.ic_workout),
+                new Habit("Do homework", R.drawable.ic_homework),
+                new Habit("Read a book", R.drawable.ic_book),
+                new Habit("Meditate for 10 minutes", R.drawable.ic_meditation),
+                new Habit("Save money today", R.drawable.ic_savemoney),
+                new Habit("Eat vegetables", R.drawable.ic_vegetable),
+                new Habit("No phone after 10PM", R.drawable.ic_phonebanned)
+        );
+
+        View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.dialog_view_habits, null);
+        ListView listView = dialogView.findViewById(R.id.habit_list_view);
+
+        HabitChoiceAdapter adapter = new HabitChoiceAdapter(getContext(), userHabits);
+        listView.setAdapter(adapter);
+
+        AlertDialog dialog = new AlertDialog.Builder(getContext())
+                .setTitle("My Habits")
+                .setView(dialogView)
+                .create();
+
+        dialog.show();
     }
 
     private void updateHabitImages() {
