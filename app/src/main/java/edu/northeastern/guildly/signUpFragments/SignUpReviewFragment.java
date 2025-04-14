5c8dde14a5c86732d810afb3e5b63186cfbc2d4b
RohanSkaria <rohan.skaria@icloud.com>
2025-04-14 14:24:30 -0400

    feat: implemented avatar selection on flow page, reflects on sign up review page and profile page.
    Signed-off-by: RohanSkaria <rohan.skaria@icloud.com>

--- app/src/main/java/edu/northeastern/guildly/signUpFragments/SignUpReviewFragment.java
+++ app/src/main/java/edu/northeastern/guildly/signUpFragments/SignUpReviewFragment.java
@@ -1,22 +1,21 @@
 package edu.northeastern.guildly.signUpFragments;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import edu.northeastern.guildly.R;
 
 public class SignUpReviewFragment extends Fragment {
     private TextView tvUsername, tvEmail, tvAboutMe, tvHabits;
-    private ImageView ivAvatar;
+    private CircleImageView ivAvatar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,7 +32,13 @@ public class SignUpReviewFragment extends Fragment {
         if (args != null) {
             tvUsername.setText(args.getString("username", ""));
             tvEmail.setText(args.getString("email", ""));
-            tvAboutMe.setText(args.getString("aboutMe", ""));
+
+            String aboutMe = args.getString("aboutMe", "");
+            if (!aboutMe.isEmpty()) {
+                tvAboutMe.setText(aboutMe);
+            } else {
+                tvAboutMe.setText(R.string.no_about_me_provided);
+            }
 
             // Format habits as a list
             ArrayList<String> habits = args.getStringArrayList("selectedHabits");
@@ -44,13 +49,29 @@ public class SignUpReviewFragment extends Fragment {
                 }
                 tvHabits.setText(habitsText.toString());
             } else {
-                tvHabits.setText("No habits selected");
+                tvHabits.setText(R.string.no_habits_selected);
             }
 
-            // Set avatar if available
-            String imageUri = args.getString("profileImageUri");
-            if (imageUri != null && !imageUri.isEmpty()) {
-                ivAvatar.setImageURI(Uri.parse(imageUri));
+            // Set avatar based on the profileImageUri string
+            String avatarType = args.getString("profileImageUri");
+            if (avatarType != null) {
+                switch (avatarType) {
+                    case "gamer":
+                        ivAvatar.setImageResource(R.drawable.gamer);
+                        ivAvatar.setCircleBackgroundColor(getResources().getColor(R.color.gamer_bg));
+                        break;
+                    case "man":
+                        ivAvatar.setImageResource(R.drawable.man);
+                        ivAvatar.setCircleBackgroundColor(getResources().getColor(R.color.man_bg));
+                        break;
+                    case "girl":
+                        ivAvatar.setImageResource(R.drawable.girl);
+                        ivAvatar.setCircleBackgroundColor(getResources().getColor(R.color.girl_bg));
+                        break;
+                    default:
+                        ivAvatar.setImageResource(R.drawable.unknown_profile);
+                        break;
+                }
             }
         }
 
